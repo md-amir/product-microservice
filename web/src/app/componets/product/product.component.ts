ee9c8fb8fb882d321f4e4bc8a6b3bbedd8e1f899
=amir <amir.ict.engg@gmail.com>
2020-11-05 22:18:38 +0600

    parent child component example

--- web/src/app/componets/product/product.component.ts
+++ web/src/app/componets/product/product.component.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
+import { Product } from '../../models/Product'
 
 @Component({
   selector: 'app-product',
@@ -8,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor(private http: HttpClient) { }
+  public products: Product[];
+
+  constructor() { }
 
   ngOnInit(): void {
 
+    this.products = [
+      {
+        "id": 1,
+        "name": "shirt",
+        "price": 3000,
+        "description": "Some text about the shirt. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem jeansum.",
+        "ratings": 1,
+        "url": "https://images.pexels.com/photos/4553134/pexels-photo-4553134.jpeg",
+      },
+      {
+        "id": 2,
+        "name": "pant",
+        "price": 2000,
+        "description": "Some text about the pant. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem jeansum.",
+        "ratings": 1,
+        "url": "https://images.pexels.com/photos/297933/pexels-photo-297933.jpeg"
+      },
+      {
+        "id": 3,
+        "name": "shari",
+        "price": 5000,
+        "description": "Some text about the shari. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem jeansum.",
+        "ratings": 1,
+        "url": "https://images.pexels.com/photos/4427641/pexels-photo-4427641.jpeg?"
+      }
+    ]
 
   }
 
---
+++ web/src/app/models/Product.ts
@@ -0,0 +1,8 @@
+export class Product {
+    id: number;
+    name: string;
+    ratings: number;
+    price: number;
+    description: string;
+    url: string
+}
