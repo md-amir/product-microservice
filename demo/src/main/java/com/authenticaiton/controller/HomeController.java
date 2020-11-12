5ffb1e1f425c26664d31339cbf44b8fe5ef06269
=amir <amir.ict.engg@gmail.com>
2020-11-12 20:42:53 +0600

    initial commit

---
+++ demo/src/main/java/com/authenticaiton/controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.authenticaiton.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index(){
+        return "home.html";
+    }
+
+    // Login form
+    @RequestMapping("/login.html")
+    public String login() {
+        return "login.html";
+    }
+
+    // Login form with error
+    @RequestMapping("/login-error.html")
+    public String loginError(Model model) {
+        model.addAttribute("loginError", true);
+        return "login.html";
+    }
+}
---
+++ demo/src/test/java/com/authenticaiton/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authenticaiton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
